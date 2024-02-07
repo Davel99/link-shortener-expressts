1bc2c71745a61d9c865d8fa0ee16514666f52091
davel99 <joelmp3@live.com>
2024-02-07 17:00:04 -0600

    Specified Database type

--- database/dbCreation.ts
+++ database/dbCreation.ts
@@ -1,16 +1,16 @@
-import sqlite3 from 'sqlite3';
+import sqlite3, { Database } from 'sqlite3';
 import fs from 'fs';
 
 const DBSOURCE = 'db.sqlite';
 
-const db = new sqlite3.Database(DBSOURCE, (err) => {
+const db : Database = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
         console.error(err.message);
         throw err;
     } else {
         console.log('Connected to the SQLite database.');
         // Read SQL statement from userTable.sql file
-        const sql = fs.readFileSync('./resources/userTable.sql', 'utf-8');
+        const sql = fs.readFileSync('./resources/database.sql', 'utf-8');
         // Execute the SQL statement
         db.exec(sql, (err) => {
             if (err) {
