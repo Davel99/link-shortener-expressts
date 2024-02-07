54b428477ddbe78c5fc16b9b9fcf82205e37d137
davel99 <joelmp3@live.com>
2024-02-06 23:21:36 -0600

    Added SQL statemetns to create tables

---
+++ resources/database.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Links (
+    id INTEGER PRIMARY KEY,
+    full_url TEXT,
+    short_url TEXT,
+    created_at TEXT
+);
+
+CREATE TABLE Statistics (
+    id INTEGER PRIMARY KEY,
+    link_id INTEGER,
+    user_agent TEXT,
+    geolocation TEXT,
+    ip_address TEXT,
+    device_type TEXT,
+    os TEXT,
+    browser TEXT,
+    timestamp TEXT,
+    FOREIGN KEY (link_id) REFERENCES Links(id)
+);
