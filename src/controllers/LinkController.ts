f564058a324f52c2f13f005533d455851ec0f286
davel99 <joelmp3@live.com>
2024-02-08 20:54:04 -0600

    Added logic to check wrong values

--- src/controllers/LinkController.ts
+++ src/controllers/LinkController.ts
@@ -1,21 +1,27 @@
 import { Response, Request } from 'express';
 import LinksService from '../services/LinkService';
 
-class LinkController{
-    private linkService : LinksService;
+class LinkController {
+    private linkService: LinksService;
 
-    constructor(service : LinksService){
-        this.linkService  = service;
+    constructor(service: LinksService) {
+        this.linkService = service;
     }
 
-    async postUser(req: Request, res: Response) : Promise<void> {
-        let response : Boolean = false;
-        console.log(req.body);
-        let { full_url, short_url } = req.body;
-        console.log(`Value of URL is ${full_url} and Short is ${short_url}`);
-        
-        response = await this.linkService.postUser( full_url, short_url );
-        res.status(200).json({response});    
+    async postUser(req: Request, res: Response): Promise<void> {
+        let response: Boolean = false;
+        let full_url : string = req.body.full_url;
+        let short_url : string = req.body.short_url;
+        if (
+            (full_url != null && short_url != null)
+        && (full_url.trim().length > 0 && short_url.trim().length > 0)
+        ) {
+            full_url = full_url.trim();
+            short_url = short_url.trim();
+            response = await this.linkService.postUser(full_url, short_url);
+            res.status(200).json({ response });
+        }
+        else res.status(400).json({ response });
     }
 
 }
