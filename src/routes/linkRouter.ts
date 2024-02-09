8f6b860c1c4506ca74981567e2486bdfcae4c07e
davel99 <joelmp3@live.com>
2024-02-08 21:23:02 -0600

    Using SecureAPI middleware in route

--- src/routes/linkRouter.ts
+++ src/routes/linkRouter.ts
@@ -1,12 +1,15 @@
 import { Response, Request } from 'express';
 import LinkController from '../controllers/LinkController';
 import LinkService from '../services/LinkService';
+import { SecureAPIMiddleware } from '../middleware/SecureApiMiddleware';
 
 const express = require('express');
 const linksRouter = express.Router();
 const linkService : LinkService = new LinkService();
 
 const linkController = new LinkController(linkService);
+
+linksRouter.use(SecureAPIMiddleware);
 
 linksRouter.post('/api/links', linkController.postUser);
 
