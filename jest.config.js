40e1be640a06cd8081d53733cd74cac64ea95964
davel99 <joelmp3@live.com>
2024-02-08 20:51:30 -0600

    Added support for ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    setupFiles: ['./jest.setup.js']
- };  
+    setupFiles: ['./jest.setup.js'],
+    testMatch: ['**/*.test.ts'], // Match test files with .test.ts extension
+    moduleNameMapper: {
+        // Map module paths to mock files
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+      },
+};
