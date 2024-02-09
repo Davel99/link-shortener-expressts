8425d8b4c5a956d4770fd550df1ab85967f32044
davel99 <joelmp3@live.com>
2024-02-08 21:20:38 -0600

    Created SEcureApi middleware

---
+++ src/middleware/SecureApiMiddleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { apiKey, apiSecret } from '../utility/envValues';
+
+export const apiKeyAuthMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { api_key , api_secret } = req.headers;
+
+  if (!apiKey || !apiSecret) {
+    return res.status(500).json({ error: 'API key or secret not configured' });
+  }
+
+  if (
+    api_key !== apiKey ||
+    api_secret !== apiSecret
+  ) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  next();
+};
