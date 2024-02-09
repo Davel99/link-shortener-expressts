d8272704a0a60f238abb52f52cc4aa46249a2663
davel99 <joelmp3@live.com>
2024-02-08 19:08:45 -0600

    Changed name for tests

--- tests/linkController.test.ts
+++ tests/linkController.test.ts
@@ -27,7 +27,31 @@ describe('LinkController', () => {
     expect(mockResponse.json).toHaveBeenCalledWith({response});
   });
 
-  it('Should return false when no short_url is provided', async () => {
+  it('Should return false when short_url is mising', async () => {
+    const mockRequest: Request = {
+        body: {
+          full_url: 'https://www.google.com/',
+          short_url: ''
+        }
+      } as unknown as Request;
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const linkService = new LinkService();
+    linkService.postUser = jest.fn().mockResolvedValue(true);
+
+    const linkController = new LinkController(linkService);
+    await linkController.postUser(mockRequest, mockResponse);
+
+    const response : Boolean = false;
+
+    expect(mockResponse.status).toHaveBeenCalledWith(400);
+    expect(mockResponse.json).toHaveBeenCalledWith({response});
+  });
+
+  it('Should return false when full_url is missing', async () => {
     const mockRequest: Request = {
         body: {
           full_url: 'https://www.google.com/',
