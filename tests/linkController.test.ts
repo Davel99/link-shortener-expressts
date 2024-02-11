1fd12270114f6ebed2df997949959fa1cc13ecbd
davel99 <joelmp3@live.com>
2024-02-10 18:00:29 -0600

    Updated test to handle error

--- tests/linkController.test.ts
+++ tests/linkController.test.ts
@@ -6,11 +6,11 @@ import appMessages from '../src/utility/appMessages';
 describe('LinkController', () => {
   it('Should return true when valid data is provided', async () => {
     const mockRequest: Request = {
-        body: {
-          full_url: 'https://www.google.com/',
-          short_url: 'goo'
-        }
-      } as unknown as Request;
+      body: {
+        full_url: 'https://www.google.com/',
+        short_url: 'goo'
+      }
+    } as unknown as Request;
     const mockResponse = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
@@ -20,21 +20,21 @@ describe('LinkController', () => {
     linkService.postLink = jest.fn().mockResolvedValue(true);
 
     const linkController = new LinkController();
-    await linkController.postLink(mockRequest, mockResponse,next);
+    await linkController.postLink(mockRequest, mockResponse, next);
 
-    const response : Boolean = true;
+    const response: Boolean = true;
 
     expect(mockResponse.status).toHaveBeenCalledWith(200);
-    expect(mockResponse.json).toHaveBeenCalledWith({response});
+    expect(mockResponse.json).toHaveBeenCalledWith({ response });
   });
 
   it('Should return Error when short_url is mising', async () => {
     const mockRequest: Request = {
-        body: {
-          full_url: 'https://www.google.com/',
-          short_url: ''
-        }
-      } as unknown as Request;
+      body: {
+        full_url: 'https://www.google.com/',
+        short_url: ''
+      }
+    } as unknown as Request;
     const mockResponse = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
@@ -42,85 +42,100 @@ describe('LinkController', () => {
     const next = jest.fn();
 
     const linkController = new LinkController();
-    linkController.postLink(mockRequest,mockResponse,next);
+    linkController.postLink(mockRequest, mockResponse, next);
 
-     // Assert that next was called
-     expect(next).toHaveBeenCalled();
+    // Assert that next was called
+    expect(next).toHaveBeenCalled();
 
-     // Get the error passed to next
-     const errorPassedToNext = next.mock.calls[0][0];
- 
-     // Assert specific properties of the error
-     expect(errorPassedToNext).toBeInstanceOf(Error);
-     expect(errorPassedToNext.message).toBe(appMessages.link.controller.invalid_params);
-     expect(errorPassedToNext.statusCode).toBe(400);
+    // Get the error passed to next
+    const errorPassedToNext = next.mock.calls[0][0];
+
+    // Assert specific properties of the error
+    expect(errorPassedToNext).toBeInstanceOf(Error);
+    expect(errorPassedToNext.message).toBe(appMessages.link.controller.invalid_params);
+    expect(errorPassedToNext.statusCode).toBe(400);
   });
 
-  // it('Should return false when full_url is missing', async () => {
-  //   const mockRequest: Request = {
-  //       body: {
-  //         short_url: 'goo'
-  //       }
-  //     } as unknown as Request;
-  //   const mockResponse = {
-  //     status: jest.fn().mockReturnThis(),
-  //     json: jest.fn(),
-  //   } as unknown as Response;
+  it('Should return Error when full_url is missing', async () => {
+    const mockRequest: Request = {
+      body: {
+        short_url: 'goo'
+      }
+    } as unknown as Request;
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+    const next = jest.fn();
 
-  //   linkService.postLink = jest.fn().mockResolvedValue(true);
+    const linkController = new LinkController();
+    linkController.postLink(mockRequest, mockResponse, next);
 
-  //   const linkController = new LinkController();
-  //   await linkController.postLink(mockRequest, mockResponse);
+    // Assert that next was called
+    expect(next).toHaveBeenCalled();
 
-  //   const response : Boolean = false;
+    // Get the error passed to next
+    const errorPassedToNext = next.mock.calls[0][0];
 
-  //   expect(mockResponse.status).toHaveBeenCalledWith(400);
-  //   expect(mockResponse.json).toHaveBeenCalledWith({response});
-  // });
+    // Assert specific properties of the error
+    expect(errorPassedToNext).toBeInstanceOf(Error);
+    expect(errorPassedToNext.message).toBe(appMessages.link.controller.invalid_params);
+    expect(errorPassedToNext.statusCode).toBe(400);
+  });
 
-  // it('Should return false when full_url is empty', async () => {
-  //   const mockRequest: Request = {
-  //       body: {
-  //         full_url: ' ',
-  //         short_url: 'goo'
-  //       }
-  //     } as unknown as Request;
-  //   const mockResponse = {
-  //     status: jest.fn().mockReturnThis(),
-  //     json: jest.fn(),
-  //   } as unknown as Response;
+  it('Should return Error when full_url is empty', async () => {
+    const mockRequest: Request = {
+      body: {
+        full_url: ' ',
+        short_url: 'goo'
+      }
+    } as unknown as Request;
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+    const next = jest.fn();
 
-  //   linkService.postLink = jest.fn().mockResolvedValue(true);
+    const linkController = new LinkController();
+    linkController.postLink(mockRequest, mockResponse, next);
 
-  //   const linkController = new LinkController();
-  //   await linkController.postLink(mockRequest, mockResponse);
+    // Assert that next was called
+    expect(next).toHaveBeenCalled();
 
-  //   const response : Boolean = false;
+    // Get the error passed to next
+    const errorPassedToNext = next.mock.calls[0][0];
 
-  //   expect(mockResponse.status).toHaveBeenCalledWith(400);
-  //   expect(mockResponse.json).toHaveBeenCalledWith({response});
-  // });
+    // Assert specific properties of the error
+    expect(errorPassedToNext).toBeInstanceOf(Error);
+    expect(errorPassedToNext.message).toBe(appMessages.link.controller.invalid_params);
+    expect(errorPassedToNext.statusCode).toBe(400);
+  });
 
-  // it('Should return false when short_url is empty', async () => {
-  //   const mockRequest: Request = {
-  //       body: {
-  //         full_url: 'google.com',
-  //         short_url: ''
-  //       }
-  //     } as unknown as Request;
-  //   const mockResponse = {
-  //     status: jest.fn().mockReturnThis(),
-  //     json: jest.fn(),
-  //   } as unknown as Response;
+  it('Should return Error when short_url is empty', async () => {
+    const mockRequest: Request = {
+      body: {
+        full_url: 'google.com',
+        short_url: ''
+      }
+    } as unknown as Request;
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+    const next = jest.fn();
 
-  //   linkService.postLink = jest.fn().mockResolvedValue(true);
+    const linkController = new LinkController();
+    linkController.postLink(mockRequest, mockResponse, next);
 
-  //   const linkController = new LinkController();
-  //   await linkController.postLink(mockRequest, mockResponse);
+    // Assert that next was called
+    expect(next).toHaveBeenCalled();
 
-  //   const response : Boolean = false;
+    // Get the error passed to next
+    const errorPassedToNext = next.mock.calls[0][0];
 
-  //   expect(mockResponse.status).toHaveBeenCalledWith(400);
-  //   expect(mockResponse.json).toHaveBeenCalledWith({response});
-  // });
+    // Assert specific properties of the error
+    expect(errorPassedToNext).toBeInstanceOf(Error);
+    expect(errorPassedToNext.message).toBe(appMessages.link.controller.invalid_params);
+    expect(errorPassedToNext.statusCode).toBe(400);
+  });
 });
