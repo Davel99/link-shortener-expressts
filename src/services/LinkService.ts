18f2bad2aca7451cabf9f01a22a94fbca53ea4b9
davel99 <joelmp3@live.com>
2024-02-11 18:29:12 -0600

    Created message for duplicated constraint

--- src/services/LinkService.ts
+++ src/services/LinkService.ts
@@ -1,12 +1,15 @@
 import { LinkDTO, voidLinkDTO } from '../dto/LinkDTO';
 import linkRepository from '../repository/linkRepository';
+import ShortenerAppError from '../utility/ShortenerAppError';
+import appMessages from '../utility/appMessages';
 
 class LinkService {
 
     async postLink(full_url: string, short_url: string): Promise<LinkDTO> {
         let response: LinkDTO = voidLinkDTO;
         let answer: boolean = await linkRepository.insert(full_url, short_url);
-        if (answer) response = await linkRepository.get(short_url);
+        if (!answer) throw new ShortenerAppError(appMessages.link.service.CREATION_ERROR, 500);
+        response = await linkRepository.get(short_url);
         return response;
     }
 
