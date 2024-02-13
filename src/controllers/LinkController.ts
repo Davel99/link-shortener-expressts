1efcd44b427356a9a83d9409e3c2814ce77f3161
davel99 <joelmp3@live.com>
2024-02-12 18:51:09 -0600

    Added error for invalid full_url

--- src/controllers/LinkController.ts
+++ src/controllers/LinkController.ts
@@ -3,20 +3,21 @@ import linkService from '../services/LinkService';
 import ShortenerAppError from '../utility/ShortenerAppError';
 import appMessages from '../utility/appMessages';
 import { LinkDTO } from '../dto/LinkDTO';
-import { strValidator } from '../utility/Validators';
+import { strValidator, urlValidator } from '../utility/Validators';
 
 class LinkController {
 
-    async redirect(req: Request, res : Response, next : NextFunction) : Promise<void> {
-        let url : string = '';
-        let short_url : string = req.params.short_url;
-        try{
-            if( strValidator(short_url) ){
+    async redirect(req: Request, res: Response, next: NextFunction): Promise<void> {
+        let url: string = '';
+        let short_url: string = req.params.short_url;
+        try {
+            if (strValidator(short_url)) {
                 url = await linkService.getUrl(short_url);
-                res.redirect(url);
+                if (!urlValidator(url)) throw new ShortenerAppError(appMessages.link.controller.invalid_fullUrl, 400);
+                else res.redirect(url);
             } else throw new ShortenerAppError(appMessages.link.controller.invalid_shortUrl, 400);
 
-        } catch(err){
+        } catch (err) {
             console.error(err);
             next(err);
         }
@@ -27,12 +28,15 @@ class LinkController {
         let full_url: string = req.body.full_url;
         let short_url: string = req.body.short_url;
         try {
-            if ( strValidator(full_url,short_url) ) {
+            if (strValidator(full_url, short_url)) {
                 full_url = full_url.trim();
                 short_url = short_url.trim();
-                response = await linkService.postLink(full_url, short_url);
-                if (response.id < 0) throw new ShortenerAppError(appMessages.gral.SQL_creation, 500);
-                else res.status(200).json(response);
+                if (!urlValidator(full_url)) throw new ShortenerAppError(appMessages.link.controller.invalid_fullUrl, 400);
+                else {
+                    response = await linkService.postLink(full_url, short_url);
+                    if (response.id < 0) throw new ShortenerAppError(appMessages.gral.SQL_creation, 500);
+                    else res.status(200).json(response);
+                }
             } else throw new ShortenerAppError(appMessages.link.controller.invalid_params, 400);
 
         } catch (error) {
@@ -41,14 +45,14 @@ class LinkController {
     }
 
     async deleteLink(req: Request, res: Response, next: NextFunction): Promise<void> {
-        let short_url : string  = req.params.short_url;
-        try{
-            if( strValidator(short_url) ){
+        let short_url: string = req.params.short_url;
+        try {
+            if (strValidator(short_url)) {
                 short_url = short_url.trim();
-                let response : boolean = await linkService.deleteLink(short_url);
+                let response: boolean = await linkService.deleteLink(short_url);
                 res.status(200).json({ response })
             } else throw new ShortenerAppError(appMessages.link.controller.invalid_shortUrl, 400);
-        } catch (error){
+        } catch (error) {
             console.error(error);
             next(error);
         }
