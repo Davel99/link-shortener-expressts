8f7503b29ae296d046d21b171b2206647aa53d5c
davel99 <joelmp3@live.com>
2024-02-21 22:04:55 -0600

    Commented creation of Statistics table

--- resources/database.sql
+++ resources/database.sql
@@ -5,15 +5,15 @@ CREATE TABLE IF NOT EXISTS Links (
     created_at TEXT DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS Statistics (
-    id INTEGER PRIMARY KEY,
-    link_id INTEGER,
-    user_agent TEXT,
-    geolocation TEXT,
-    ip_address TEXT,
-    device_type TEXT,
-    os TEXT,
-    browser TEXT,
-    timestamp TEXT DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (link_id) REFERENCES Links(id)
-);
+-- CREATE TABLE IF NOT EXISTS Statistics (
+--     id INTEGER PRIMARY KEY,
+--     link_id INTEGER,
+--     user_agent TEXT,
+--     geolocation TEXT,
+--     ip_address TEXT,
+--     device_type TEXT,
+--     os TEXT,
+--     browser TEXT,
+--     timestamp TEXT DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (link_id) REFERENCES Links(id)
+-- );
